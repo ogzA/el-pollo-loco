7c86f572ae72932eebb2274065e2752686417d2d
ogzA <akankan.oguz@gmail.com>
2026-08-30 22:15:37 +0200

    feat: imageHub for the game

---
+++ models/image-hub.class.js
@@ -0,0 +1,51 @@
+export class ImageHub {
+	static PEPE = {
+		move: [],
+		idle: ["0", "1"],
+		long_idle: [],
+		jump: [],
+		hurt: [],
+		dead: [],
+	};
+
+	static FINALBOSS = {
+		move: [],
+		alert: [],
+		attack: [],
+		hurt: [],
+	};
+
+	static CHICKEN = {
+		move: [],
+		dead: [],
+	};
+
+	static CLOUD = {
+		move: [],
+	};
+
+	static BACKGROUND = {
+		plain: [],
+		red: [],
+		color: [],
+		sky: [],
+	};
+
+	static BARS = {
+		health: [],
+		coins: [],
+		bottles: [],
+	};
+
+	static BOTTLES = {
+		left: [],
+		right: [],
+		straight: [],
+		flying: [],
+		splash: [],
+	};
+
+	static COINS = {
+		ground: [],
+	};
+}
