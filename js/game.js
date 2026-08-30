e470c4d7cbdaefc83701443bf97faf4e1671cd79
ogzA <akankan.oguz@gmail.com>
2026-08-30 22:20:50 +0200

    feat: add Chicken class and game.js

---
+++ js/game.js
@@ -0,0 +1,8 @@
+import { Chicken } from "../models/chicken.class.js";
+import { ImageHub } from "../models/image-hub.class.js";
+
+const chicken = new Chicken();
+
+console.log(chicken);
+
+console.log(ImageHub.PEPE.idle[1]);
---
+++ models/chicken.class.js
@@ -0,0 +1,8 @@
+import { MovableObject } from "./movable-object.class.js";
+
+export class Chicken extends MovableObject {
+	constructor() {
+		super();
+		console.log("Hello");
+	}
+}
