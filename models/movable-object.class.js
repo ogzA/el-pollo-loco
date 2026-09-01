17b3568da00e0d1bece366e578097cc9149bd116
ogzA <akankan.oguz@gmail.com>
2026-09-01 10:23:51 +0200

    feat: add new variables for MovableObject

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,12 @@
 export class MovableObject {
-	x;
-	y;
+	x = 120;
+	y = 400;
 	img;
+	height = 150;
+	width = 100;
+	imageCache = {};
+	currentImage = 0;
+	speed = 0.15;
 
 	moveRight() {
 		console.log("is moving to right ...");
