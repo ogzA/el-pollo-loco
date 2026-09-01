330e3aab769255f8fdb3ada75b49cb4b74c3bfe8
ogzA <akankan.oguz@gmail.com>
2026-09-01 10:24:52 +0200

    test: test enemies

--- js/game.js
+++ js/game.js
@@ -1,8 +1,21 @@
+import { Character } from "../models/character.class.js";
 import { Chicken } from "../models/chicken.class.js";
-import { ImageHub } from "../models/image-hub.class.js";
 
-const chicken = new Chicken();
+let canvas;
+let ctx;
+const character = new Character();
+const enemies = [new Chicken(), new Chicken(), new Chicken()];
 
-console.log(chicken);
+function init() {
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
 
-console.log(ImageHub.PEPE.idle[1]);
+	console.log("My Character is", character);
+}
+
+window.addEventListener("load", init);
+
+// to test in browser
+/* window.character = character; */
+
+window.enemies = enemies;
