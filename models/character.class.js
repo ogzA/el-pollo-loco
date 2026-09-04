5a5728e373ede1b6e4f23f76a991d47a20543127
ogzA <akankan.oguz@gmail.com>
2026-09-05 00:32:54 +0200

    feat: implement character walking animation
    Apply the new image caching system to the `Character` class and set up an animation interval to cycle through walking sprites.

--- models/character.class.js
+++ models/character.class.js
@@ -2,12 +2,28 @@ import { ImageHub } from "./image-hub.class.js";
 import { MovableObject } from "./movable-object.class.js";
 
 export class Character extends MovableObject {
+	IMAGES_WALKING = [];
+	IMAGES_WALKING = ImageHub.PEPE.move;
+
 	constructor() {
-		super().loadImage(ImageHub.PEPE.move[0]);
+		super();
+		this.loadImage(this.IMAGES_WALKING[0]);
+		this.loadImages(this.IMAGES_WALKING);
 
 		this.height = 350;
 		this.width = 150;
 		this.y = 90;
+
+		this.animate();
+	}
+
+	animate() {
+		setInterval(() => {
+			const i = this.currentImage % this.IMAGES_WALKING.length;
+			const path = this.IMAGES_WALKING[i];
+			this.img = this.imageCache[path];
+			this.currentImage++;
+		}, 100);
 	}
 
 	moveRight() {
