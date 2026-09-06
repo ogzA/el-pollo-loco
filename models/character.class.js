9e2571fcdfd7305cb6e5b881c2971b412044ae18
ogzA <akankan.oguz@gmail.com>
2026-09-06 13:45:21 +0200

    feat: implement sprite flipping for leftward movement
    Add dynamic canvas context transformations to flip character sprites horizontally when moving left, preventing moonwalking visuals.
    
    This commit:
    * Introduces a boolean `otherDirection` flag in `MovableObject` to track facing state.
    * Updates `Character.animate()` to toggle `otherDirection` based on left/right keyboard input.
    * Adds `flipImage` and `flipImageBack` helper methods in `World` to handle canvas context translation and scaling during the render loop.
    * Conditionally applies these canvas transformations in `addToMap` before drawing the sprite.

--- models/character.class.js
+++ models/character.class.js
@@ -23,9 +23,11 @@ export class Character extends MovableObject {
 		setInterval(() => {
 			if (this.world.keyboard.RIGHT) {
 				this.x += this.speed;
+				this.otherDirection = false;
 			}
 			if (this.world.keyboard.LEFT) {
 				this.x -= this.speed;
+				this.otherDirection = true;
 			}
 		}, 1000 / 60);
 
--- models/world.class.js
+++ models/world.class.js
@@ -59,7 +59,27 @@ export class World {
 	}
 
 	addToMap(mo) {
+		if (mo.otherDirection) {
+			this.flipImage(mo);
+		}
+
 		this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
+		if (mo.otherDirection) {
+			this.flipImageBack(mo);
+		}
+	}
+
+	flipImage(mo) {
+		this.ctx.save();
+		this.ctx.translate(mo.width, 0);
+		this.ctx.scale(-1, 1);
+		mo.x = mo.x * -1;
+	}
+
+	flipImageBack(mo) {
+		mo.x = mo.x * -1;
+		this.ctx.restore();
 	}
 
 	clearCanvas(canvas) {
