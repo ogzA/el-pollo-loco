f8bcd18dc17fae3753498e465f29e8c78f5772c7
ogzA <akankan.oguz@gmail.com>
2026-09-12 02:43:12 +0200

    fix(bottle-bar): correct image index resolution order
    The if/else chain checked thresholds in ascending order, so the
    first matching branch always won and higher percentages never
    reached their intended image. Values between thresholds returned
    undefined, which crashed drawImage.

--- models/bottle-bar.class.js
+++ models/bottle-bar.class.js
@@ -4,7 +4,7 @@ import { ImageHub } from "./image-hub.class.js";
 export class BottleBar extends DrawableObject {
 	STATUS_IMAGES = ImageHub.STATUS_BAR.bottles;
 
-	percentage = 40;
+	percentage = 0;
 
 	constructor() {
 		super();
@@ -13,28 +13,29 @@ export class BottleBar extends DrawableObject {
 		this.y = 100;
 		this.width = 200;
 		this.height = 70;
-		this.setPercentage(40);
+		this.setPercentage(0);
 	}
 
 	setPercentage(percentage) {
 		this.percentage = percentage; // => 0 ... 5
 		let path = this.STATUS_IMAGES[this.resolveImageIndex()];
+		console.log(percentage, path);
 		this.img = this.imageCache[path];
 	}
 
 	resolveImageIndex() {
-		if (this.percentage == 40) {
-			return 4;
-		} else if (this.percentage > 0) {
-			return 1;
-		} else if (this.percentage > 20) {
-			return 2;
-		} else if (this.percentage > 40) {
-			return 3;
-		} else if (this.percentage > 60) {
-			return 4;
-		} else if (this.percentage > 80) {
+		if (this.percentage == 100) {
 			return 5;
+		} else if (this.percentage > 80) {
+			return 4;
+		} else if (this.percentage > 60) {
+			return 3;
+		} else if (this.percentage > 40) {
+			return 2;
+		} else if (this.percentage > 20) {
+			return 1;
+		} else {
+			return 0;
 		}
 	}
 }
