bef0779efc000b9516ae5d4786931636c671746c
ogzA <akankan.oguz@gmail.com>
2026-09-14 02:54:33 +0200

    chore: create level objects in initLevel instead of on import

--- levels/level1.js
+++ levels/level1.js
@@ -7,100 +7,112 @@ import { FinalBoss } from "../models/final-boss.class.js";
 import { BottleObject } from "../models/bottle-object.class.js";
 import { CoinObject } from "../models/coin-object.js";
 
-const enemies = [];
-for (let i = 0; i < 7; i++) {
-	enemies.push(new Chicken(i));
-}
-for (let i = 0; i < 7; i++) {
-	enemies.push(new ChickenSmall(i));
-}
-enemies.push(new FinalBoss());
+export let level1;
 
-const collectables = [];
-for (let i = 0; i < 12; i++) {
-	collectables.push(new BottleObject());
-}
-for (let i = 0; i < 20; i++) {
-	collectables.push(new CoinObject());
+export function initLevel() {
+	level1 = new Level(
+		createEnemies(),
+		[new Cloud()],
+		backgroundObjects,
+		createCollectables(),
+	);
 }
 
-export const level1 = new Level(
-	enemies,
-	[new Cloud()],
-	[
-		new BackgroundObject("./assets/img/5_background/layers/air.png", -1024),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/3_third_layer/2.png",
-			-1024,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/2_second_layer/2.png",
-			-1024,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/1_first_layer/2.png",
-			-1024,
-		),
+function createEnemies() {
+	const enemies = [];
+	for (let i = 0; i < 7; i++) {
+		enemies.push(new Chicken(i));
+	}
+	for (let i = 0; i < 7; i++) {
+		enemies.push(new ChickenSmall(i));
+	}
+	enemies.push(new FinalBoss());
+	return enemies;
+}
 
-		new BackgroundObject("./assets/img/5_background/layers/air.png", 0),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/3_third_layer/1.png",
-			0,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/2_second_layer/1.png",
-			0,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/1_first_layer/1.png",
-			0,
-		),
-		new BackgroundObject("./assets/img/5_background/layers/air.png", 1024),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/3_third_layer/2.png",
-			1024,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/2_second_layer/2.png",
-			1024,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/1_first_layer/2.png",
-			1024,
-		),
+function createCollectables() {
+	const collectables = [];
+	for (let i = 0; i < 12; i++) {
+		collectables.push(new BottleObject());
+	}
+	for (let i = 0; i < 20; i++) {
+		collectables.push(new CoinObject());
+	}
+	return collectables;
+}
 
-		new BackgroundObject(
-			"./assets/img/5_background/layers/air.png",
-			1024 * 2,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/3_third_layer/1.png",
-			1024 * 2,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/2_second_layer/1.png",
-			1024 * 2,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/1_first_layer/1.png",
-			1024 * 2,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/air.png",
-			1024 * 3,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/3_third_layer/2.png",
-			1024 * 3,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/2_second_layer/2.png",
-			1024 * 3,
-		),
-		new BackgroundObject(
-			"./assets/img/5_background/layers/1_first_layer/2.png",
-			1024 * 3,
-		),
-	],
-	collectables,
-);
+const backgroundObjects = [
+	new BackgroundObject("./assets/img/5_background/layers/air.png", -1024),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/3_third_layer/2.png",
+		-1024,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/2_second_layer/2.png",
+		-1024,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/1_first_layer/2.png",
+		-1024,
+	),
+
+	new BackgroundObject("./assets/img/5_background/layers/air.png", 0),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/3_third_layer/1.png",
+		0,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/2_second_layer/1.png",
+		0,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/1_first_layer/1.png",
+		0,
+	),
+	new BackgroundObject("./assets/img/5_background/layers/air.png", 1024),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/3_third_layer/2.png",
+		1024,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/2_second_layer/2.png",
+		1024,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/1_first_layer/2.png",
+		1024,
+	),
+
+	new BackgroundObject(
+		"./assets/img/5_background/layers/air.png",
+		1024 * 2,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/3_third_layer/1.png",
+		1024 * 2,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/2_second_layer/1.png",
+		1024 * 2,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/1_first_layer/1.png",
+		1024 * 2,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/air.png",
+		1024 * 3,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/3_third_layer/2.png",
+		1024 * 3,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/2_second_layer/2.png",
+		1024 * 3,
+	),
+	new BackgroundObject(
+		"./assets/img/5_background/layers/1_first_layer/2.png",
+		1024 * 3,
+	),
+];
